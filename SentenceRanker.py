133337cf205d928fcb3050c6ce7ca8c59567432a
Duyemo <45041539+Duyemo@users.noreply.github.com>
2024-04-02 02:05:51 +0200

    emotions code
    code for getting sentences ranked based on emotion

---
+++ SentenceRanker.py
@@ -0,0 +1,47 @@
+import sys
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+class SentenceRanker:
+    def __init__(self, sentences):
+        self.sentences = sentences
+        self.vectorizer = CountVectorizer().fit(sentences)
+        self.vectors = self.vectorizer.transform(sentences)
+
+    """rank all sentences only on similarity to the query"""
+    def rank_sentences(self, query):
+        query_vector = self.vectorizer.transform([query])
+        similarity_scores = cosine_similarity(query_vector, self.vectors)
+        ranked_sentences_indices = similarity_scores.argsort()[0][::-1]
+        ranked_sentences = [(self.sentences[idx], similarity_scores[0, idx]) for idx in ranked_sentences_indices]
+        return ranked_sentences
+
+    """ rank all sentences on similarity to the query and on the emotion score, based on labels given to sentences.
+        The emotion score is calculated for each sentence. The emotion score is the probability of the query belonging
+        to the emotion corresponding to the label given to a sentence."""
+    def rank_sentences_emotions_labels(self, query, emotions, labels):
+        query_vector = self.vectorizer.transform([query])
+        similarity_scores = cosine_similarity(query_vector, self.vectors)
+        for i,score in enumerate(similarity_scores[0]):
+            label = labels[i]
+            emotion_score = emotions[label]
+            similarity_scores[0, i] *= emotion_score
+        ranked_sentences_indices = similarity_scores.argsort()[0][::-1]
+        ranked_sentences = [(self.sentences[idx], similarity_scores[0, idx]) for idx in ranked_sentences_indices]
+        return ranked_sentences
+
+    """ rank all sentences on similarity to the query and on the emotion score, with unlabeled data
+        The emotion score is calculated for each sentence. The emotion score is calculated by taking the probability
+        vectors for emotions of both the query and a sentence, and taking the dot product of these 2 vectors."""
+    def rank_sentences_emotions_unlabeled(self, query, emotions, labels):
+        query_vector = self.vectorizer.transform([query])
+        similarity_scores = cosine_similarity(query_vector, self.vectors)
+        for i, score in enumerate(similarity_scores[0]):
+            label = labels[i]
+            emotion_score = np.dot(label, emotions)
+            similarity_scores[0, i] *= emotion_score
+        ranked_sentences_indices = similarity_scores.argsort()[0][::-1]
+        ranked_sentences = [(self.sentences[idx], similarity_scores[0, idx]) for idx in ranked_sentences_indices]
+        return ranked_sentences
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import csv
+import sys
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+# from bm25model import OkapiBM25Model
+from sklearn.feature_extraction.text import TfidfVectorizer
+# from gensim.corpora import Dictionary
+from SentenceRanker import SentenceRanker
 
 training_tweets = list()
 training_labels = list()
@@ -11,6 +16,7 @@ test_tweets = list()
 test_labels = list()
 validation_tweets = list()
 validation_labels = list()
+predicted_labels = []
 
 
 def train_model():
@@ -52,12 +58,101 @@ def find_tweets_with_same_label(label):
 
     return [s for s, l in zip(tweets, labels) if l == label]
 
+
+
+# def runBM25(query):
+#     tweets = training_tweets + test_tweets + validation_tweets
+#     # print(tweets)
+#     split_tweets = [tweet.split() for tweet in tweets]
+#     # print(split_tweets)
+#     dictionary = Dictionary(split_tweets)  # fit dictionary
+#     # print(dictionary)
+#     corpus = [dictionary.doc2bow(line) for line in split_tweets]
+#
+#     bm25 = OkapiBM25Model(corpus=corpus, dictionary=dictionary)
+#
+#
+#
+#     print("start getting document scores")
+#     document_scores = bm25[query]
+#     print("start ranking")
+#
+#     ranked_documents = sorted(enumerate(document_scores), key=lambda x: x[1], reverse=True)
+#     print(ranked_documents)
+#     print("OK")
+#     sys.exit(0)
+#
+#     # Calculate BM25 scores for documents
+#     # scores = bm25.get_scores(query, tweets)
+#     #
+#     # ranked_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)
+#     # ranked_documents = [tweets[i] for i in ranked_indices]
+#     #
+#     # print("Ranked Documents:")
+#     # for i, document in enumerate(ranked_documents):
+#     #     if i >10:
+#     #         break
+#     #     print(document)
+
+def rankTweetsLabels(query):
+    emotion = trained_pipeline.predict_proba([query])[0]
+    tweets = training_tweets
+    labels = training_labels
+
+    # print(emotion)
+    ranker = SentenceRanker(tweets)
+    ranked_results = ranker.rank_sentences(query)
+    ranked_results_labels = ranker.rank_sentences_emotions_labels(query,emotion,labels)
+
+    print("_________________________________________________________________________________")
+    print("Without emotion: ")
+    for i, (sentence, similarity_score) in enumerate(ranked_results):
+        print(f"{sentence} (Similarity Score: {similarity_score:.4f})")
+        if i >=10:
+            break
+
+    print("_________________________________________________________________________________")
+    print("With labeled emotions: ")
+    for i, (sentence, similarity_score) in enumerate(ranked_results_labels):
+        print(f"{sentence} (Similarity Score: {similarity_score:.4f})")
+        if i >= 10:
+            break
+
+
+def rankTweetsUnlabeled(query):
+    emotion = trained_pipeline.predict_proba([query])[0]
+    tweets = training_tweets
+
+    if predicted_labels == []:
+        for tweet in tweets:
+            predicted_labels.append(trained_pipeline.predict_proba([tweet])[0])
+
+    # print(emotion)
+    ranker = SentenceRanker(tweets)
+    ranked_results = ranker.rank_sentences_emotions_unlabeled(query,emotion,predicted_labels)
+
+    print("_________________________________________________________________________________")
+    print("With unlabeled emotions")
+    for i, (sentence, similarity_score) in enumerate(ranked_results):
+        print(f"{sentence} (Similarity Score: {similarity_score:.4f})")
+        if i >=10:
+            break
+
+
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
     trained_pipeline = train_model()
     while True:
         query = input('Enter query: ')
-        emotion = trained_pipeline.predict([query])
-        print(f'Emotion: {emotion}')
-        tweets_with_same_label = find_tweets_with_same_label(emotion)
-        print(tweets_with_same_label)
+        # emotion = trained_pipeline.predict([query])
+
+        # print(f'Emotion: {emotions}')
+        rankTweetsLabels(query)
+        rankTweetsUnlabeled(query)
+
+    #     tweets_with_same_label = find_tweets_with_same_label(emotion)
+    #     print(tweets_with_same_label)
